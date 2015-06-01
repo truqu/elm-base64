36a47d689b336d119ae9ed750b7449deba5b01fb
Michel Rijnders <michel.rijnders@truqu.com>
2015-06-01 13:48:03 +0200

    Tweak code layout

--- BitList.elm
+++ BitList.elm
@@ -3,13 +3,14 @@ module BitList where
 import List
 import List exposing (append)
 
+
 type Bit = On | Off
+
 fromNumber : Int -> List Bit
 fromNumber int =
   if | int == 0 -> []
      | int % 2 == 1 -> fromNumber(int//2) `append` [On]
      | int % 2 == 0 -> fromNumber(int//2) `append` [Off]
-
 
 fromNumberWithSize : Int -> Int -> List Bit
 fromNumberWithSize number size =
@@ -18,7 +19,6 @@ fromNumberWithSize number size =
     paddingSize = size - (List.length(bitList))
   in
     (List.repeat paddingSize Off) `append` bitList
-
 
 fromByte : Int -> List Bit
 fromByte byte = fromNumberWithSize byte 8
@@ -34,7 +34,6 @@ toByteReverse bitList = case bitList of
 
 partition : Int -> List Bit -> List(List Bit)
 partition size list =
-  if ((List.length list)<=size) then
-    [list]
-  else
-    (List.take size list) :: (partition size (List.drop size list))
+  if ((List.length list)<=size)
+  then [list]
+  else (List.take size list) :: (partition size (List.drop size list))
