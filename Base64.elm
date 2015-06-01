76eeacd033f52b205989d5500d8f39743076aa3a
Michel Rijnders <michel.rijnders@truqu.com>
2015-06-01 14:31:56 +0200

    Tweak code layout

--- Base64.elm
+++ Base64.elm
@@ -24,69 +24,81 @@ isBase64Char char = Dict.member char base64Map
 
 isValid : String -> Bool
 isValid string =
-  String.all isBase64Char <| if | String.endsWith "==" string -> String.dropRight 2 string
-                                | String.endsWith "=" string -> String.dropRight 1 string
-                                | otherwise -> string
+  String.all isBase64Char
+          <| if | String.endsWith "==" string -> String.dropRight 2 string
+                | String.endsWith "=" string  -> String.dropRight 1 string
+                | otherwise                   -> string
 
 insertPairIntoDict : (v , comparable) -> Dict comparable v -> Dict comparable v
-insertPairIntoDict (value , key) dict = Dict.insert key value dict
-
+insertPairIntoDict (value , key) dict =
+  Dict.insert key value dict
 
 toBase64Char : Int -> Char
-toBase64Char index = Maybe.withDefault '!' (Array.get index base64CharsArray)
+toBase64Char index =
+  Maybe.withDefault '!' (Array.get index base64CharsArray)
 
 fromBase64Char : Char -> Int
-fromBase64Char char = Maybe.withDefault 0 (Dict.get char base64Map)
+fromBase64Char char =
+  Maybe.withDefault 0 (Dict.get char base64Map)
 
 toBitList : List Int -> List(Bit)
-toBitList list = List.foldr List.append [] (List.map BitList.fromByte list)
+toBitList list =
+  List.foldr List.append [] (List.map BitList.fromByte list)
 
 partitionBits : List Int -> List Int
-partitionBits list = List.map BitList.toByte (BitList.partition 6 (toBitList list))
+partitionBits list =
+  List.map BitList.toByte (BitList.partition 6 (toBitList list))
 
 toChars : (Int,Int,Int) -> List Char
-toChars (a,b,c) = case (a,b,c) of
-  (a, -1, -1) -> (dropLast 2 (List.map toBase64Char (partitionBits [a,0,0]))) `append` ['=','=']
-  (a, b, -1) -> (dropLast 1 (List.map toBase64Char (partitionBits [a,b,0]))) `append` ['=']
-  (a, b, c) -> (List.map toBase64Char (partitionBits [a,b,c]))
+toChars (a,b,c) =
+  case (a,b,c) of
+    (a, -1, -1) -> (dropLast 2 (List.map toBase64Char (partitionBits [a,0,0]))) `append` ['=','=']
+    (a, b, -1)  -> (dropLast 1 (List.map toBase64Char (partitionBits [a,b,0]))) `append` ['=']
+    (a, b, c)   -> (List.map toBase64Char (partitionBits [a,b,c]))
 
 dropLast : Int -> List a -> List a
-dropLast number list = List.reverse list |> List.drop number |> List.reverse
+dropLast number list =
+  List.reverse list |> List.drop number |> List.reverse
 
 toCharList : List (Int,Int,Int) -> List Char
-toCharList bitList = List.concatMap toChars bitList
+toCharList bitList =
+  List.concatMap toChars bitList
 
 toTupleList : List Int -> List (Int, Int, Int)
-toTupleList list = case list of
-  a :: b :: c :: l -> (a, b, c) :: toTupleList(l)
-  a :: b :: [] -> [(a, b , -1)]
-  a :: [] -> [(a, -1, -1)]
-  [] -> []
-  _ -> [(-1, -1, -1)]
+toTupleList list =
+  case list of
+    a :: b :: c :: l -> (a, b, c) :: toTupleList(l)
+    a :: b :: []     -> [(a, b , -1)]
+    a :: []          -> [(a, -1, -1)]
+    []               -> []
+    _                -> [(-1, -1, -1)]
 
 toAsciiList : String -> List(Int)
-toAsciiList string = List.map (asciiToInt) (String.toList string)
+toAsciiList string =
+  List.map (asciiToInt) (String.toList string)
 
 toBase64BitList : String -> List(Bit)
 toBase64BitList string =
   let
     endingEquals = if | (String.endsWith "==" string) -> 2
-                      | (String.endsWith "=" string) -> 1
-                      | otherwise -> 0
+                      | (String.endsWith "=" string)  -> 1
+                      | otherwise                     -> 0
     stripped = String.toList (String.dropRight endingEquals string)
     numberList = List.map base64ToInt stripped
   in
     dropLast (endingEquals*2) <| List.concatMap (flip BitList.fromNumberWithSize <| 6) numberList
 
 base64ToInt : Char -> Int
-base64ToInt char = case Dict.get char base64Map of
-  Just(value) -> value
-  _ -> -1
+base64ToInt char =
+  case Dict.get char base64Map of
+    Just(value) -> value
+    _           -> -1
 
 asciiToInt : Char -> Int
-asciiToInt char = case Ascii.toInt char of
-  Result.Ok(value) -> value
-  _ -> -1
+asciiToInt char =
+  case Ascii.toInt char of
+    Result.Ok(value) -> value
+    _                -> -1
 
 encode : String -> Result String String
 encode s =
@@ -95,10 +107,11 @@ encode s =
   else Result.Ok (toAsciiList s |> toTupleList |> toCharList |> String.fromList)
 
 resultUnfold : List(Result a b) -> List b
-resultUnfold list = case list of
-  [] -> []
-  Result.Ok(head) :: tail -> head::resultUnfold(tail)
-  Result.Err(err) :: tail -> []
+resultUnfold list =
+  case list of
+    []                      -> []
+    Result.Ok(head) :: tail -> head :: resultUnfold(tail)
+    Result.Err(err) :: tail -> []
 
 decode : String -> Result String String
 decode s =
