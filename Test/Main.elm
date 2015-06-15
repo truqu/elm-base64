f93dbea138e844221c78bc008a9ea69794e8dc33
Jacques Bouman <jacques.bouman@gmail.com>
2015-06-15 23:54:11 +0200

    Added elm-check test to test-suite

--- Test/Main.elm
+++ Test/Main.elm
@@ -3,10 +3,14 @@ module Main where
 import Test.AsciiTest as AsciiTest
 import Test.Base64Test as Base64Test
 import Test.BitListTest as BitListTest
-import IO.IO exposing (..)
+import Test.Base64Check as Base64Check
+import IO.IO exposing ((>>>))
 import IO.Runner exposing (Request, Response, run)
 import ElmTest.Test exposing (Test,suite)
 import ElmTest.Runner.Console exposing (runDisplay)
+import Test.Console as Console
+import Check exposing (quickCheck)
+import Check
 
 tests : Test
 tests = suite "Main" [
@@ -15,7 +19,16 @@ tests = suite "Main" [
   AsciiTest.tests
   ]
 
+allChecks : Check.Claim
+allChecks =
+  Check.suite "Main"
+    [ Base64Check.checks ]
+
 port requests : Signal Request
-port requests = run responses (runDisplay tests)
+port requests =
+  run responses (
+    Console.runDisplay (quickCheck allChecks) >>>
+    runDisplay tests
+  )
 
 port responses : Signal Response
