c3811bb1c29687265655224774cff1573d70ad88
Jacques Bouman <jacques.bouman@gmail.com>
2015-06-16 08:26:09 +0200

    Small refactor in io execution. Still does not work

--- Test/Main.elm
+++ Test/Main.elm
@@ -4,7 +4,7 @@ import Test.AsciiTest as AsciiTest
 import Test.Base64Test as Base64Test
 import Test.BitListTest as BitListTest
 import Test.Base64Check as Base64Check
-import IO.IO exposing ((>>>))
+import IO.IO exposing (seq)
 import IO.Runner exposing (Request, Response, run)
 import ElmTest.Test exposing (Test,suite)
 import ElmTest.Runner.Console exposing (runDisplay)
@@ -26,9 +26,10 @@ allChecks =
 
 port requests : Signal Request
 port requests =
-  run responses (
-    Console.runDisplay (quickCheck allChecks) >>>
-    runDisplay tests
-  )
+  let
+    testResponse = runDisplay tests
+    checkResponse = Console.runDisplay (quickCheck allChecks)
+  in
+    run responses (seq testResponse checkResponse)
 
 port responses : Signal Response
