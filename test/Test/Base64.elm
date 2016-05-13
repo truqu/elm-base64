cd7bd9cf6cae06913f84b63241ea0e864f61e585
Martin Janiczek <martin@janiczek.cz>
2016-05-13 21:27:18 +0200

    Upgrade to 0.17

--- test/Test/Base64.elm
+++ test/Test/Base64.elm
@@ -1,30 +1,31 @@
-module Test.Base64 (tests) where
+module Test.Base64 exposing (tests)
 
 import Base64
 import ElmTest exposing (Test, assertEqual, defaultTest, suite)
 
 
-encodeTest : (String, String) -> Test
-encodeTest (string, base64) =
-  defaultTest (assertEqual  (Result.Ok base64) (Base64.encode string))
+encodeTest : ( String, String ) -> Test
+encodeTest ( string, base64 ) =
+    defaultTest (assertEqual (Result.Ok base64) (Base64.encode string))
 
 
-decodeTest : (String, String) -> Test
-decodeTest (string, base64) =
-  defaultTest (assertEqual (Result.Ok string) (Base64.decode base64))
+decodeTest : ( String, String ) -> Test
+decodeTest ( string, base64 ) =
+    defaultTest (assertEqual (Result.Ok string) (Base64.decode base64))
 
 
 examples =
-  [ ("aaa", "YWFh")
-  , ("my updated file contents", "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz")
-  , ("a", "YQ==")
-  , ("aa", "YWE=")
-  , ("Elm is Cool", "RWxtIGlzIENvb2w=")
-  ]
+    [ ( "aaa", "YWFh" )
+    , ( "my updated file contents", "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz" )
+    , ( "a", "YQ==" )
+    , ( "aa", "YWE=" )
+    , ( "Elm is Cool", "RWxtIGlzIENvb2w=" )
+    ]
 
 
 tests : Test
 tests =
-  suite "Base64" [ suite "encode" <| List.map encodeTest examples
-                 , suite "decode" <| List.map decodeTest examples
-                 ]
+    suite "Base64"
+        [ suite "encode" <| List.map encodeTest examples
+        , suite "decode" <| List.map decodeTest examples
+        ]
