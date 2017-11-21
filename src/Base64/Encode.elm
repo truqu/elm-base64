66f0c6e02e6cf2f6fd1af481b88877ee3e6c9a29
ilias <mail@ilias.xyz>
2017-11-21 10:52:29 +0100

    💀  dead code, begone

--- src/Base64/Encode.elm
+++ src/Base64/Encode.elm
@@ -18,14 +18,6 @@ initial =
     ( Nothing, ( "", 0, 0 ) )
 
 
-notZero : Int -> Int
-notZero i =
-    if i == 0 then
-        -1
-    else
-        i
-
-
 wrapUp : Accumulator -> String
 wrapUp ( _, ( res, cnt, acc ) ) =
     case cnt of
